5a07847a556685ce6b5df105a096b36670102917
Sunday Odong <sodong@pridemicrofinance.co.ug>
2024-09-18 17:10:05 +0300

    fix layout

--- src/components/appBar/index.tsx
+++ src/components/appBar/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import AppBar from '@mui/material/AppBar';
@@ -11,16 +11,26 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import { Outlet, useLocation, useNavigate } from 'react-router';
 import { sideBarItems } from './sideBarElements';
-import { Collapse } from '@mui/material';
+import { Avatar, Collapse } from '@mui/material';
 import { ExpandLess, ExpandMore } from '@mui/icons-material';
 import { blue } from '@mui/material/colors';
 import HandleRoutes from './HandleRoutes';
+import MaleLogo from '../../statics/images/male.jpg';
+import FemaleLogo from '../../statics/images/female.webp';
+import { UserContext } from '../../context/UserContext';
+import PopoverComponent from '../forms/Popover';
+import InfoIcon from '@mui/icons-material/Info';
+import ModeEditIcon from '@mui/icons-material/ModeEdit';
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
 
-const drawerWidth = 250;
+const drawerWidth = 200;
 
 export default function ApplicationDrawer() {
     const navigate = useNavigate();
     const { pathname } = useLocation();
+    const { user } = useContext(UserContext);
+
+    console.log(user, "information!!")
 
     const {
         handleClick,
@@ -37,8 +47,21 @@ export default function ApplicationDrawer() {
             <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
                 <Toolbar>
                     <Typography variant="h6" noWrap component="div">
-                        Pride Microfinance
+                        Assets Management
                     </Typography>
+                    <Avatar src={MaleLogo} sx={{ height: 45, width: 45, ml: "auto", cursor: "pointer" }} />
+                    <PopoverComponent
+                        moduleID={user.id}
+                        buttonText='Profile'
+                        handleOptionClicked={() => console.log("Clicked")}
+                        options={
+                            [
+                                { value: "deactivate", label: "Deactivate", icon: <InfoIcon fontSize='small' color='error' /> },
+                                { value: "update", label: "Update", icon: <ModeEditIcon fontSize='small' color='info' /> },
+                                { value: "read", label: "View Details", icon: <RemoveRedEyeIcon fontSize='small' color='inherit' /> }
+                            ]
+
+                        } />
                 </Toolbar>
             </AppBar>
             <Drawer
