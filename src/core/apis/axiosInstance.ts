5af38ab4f6b0bb94080e9f5a03bc5c5cd699151e
Sunday Odong <sodong@pridemicrofinance.co.ug>
2024-09-23 12:11:06 +0300

    add axios configs

---
+++ src/core/apis/axiosInstance.ts
@@ -0,0 +1,71 @@
+import axios, { AxiosResponse, InternalAxiosRequestConfig } from "axios";
+
+const status = (response: AxiosResponse): Promise<AxiosResponse> => {
+    if (response.status >= 200 && response.status < 300) {
+        return Promise.resolve(response);
+    }
+    return Promise.reject(response);
+}
+
+export const service = (() => {
+    const api = axios.create({
+        baseURL: 'http://localhost:8080/api',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        },
+        withCredentials: true
+    });
+
+    // Add the JWT token to each request
+    const onRequest = (config: InternalAxiosRequestConfig): InternalAxiosRequestConfig => {
+        const token = sessionStorage.getItem('jwtToken');
+
+        if (token) {
+            config.headers['Authorization'] = `Bearer ${token}`;
+        }
+        
+        return config;
+    }
+
+    const onRequestError = (error: any): Promise<any> => {
+        return Promise.reject(error);
+    }
+
+    const onResponse = (response: AxiosResponse): Promise<AxiosResponse> => {
+        return status(response);
+    }
+
+    const onResponseError = async (error: any): Promise<any> => {
+        const originalRequest = error.config;
+
+        if (error.response && error.response.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+
+            try {
+                // Assuming the refresh token is also stored in session storage
+                const refreshToken = sessionStorage.getItem('refreshToken');
+                const response = await api.post('/auth/refresh', { token: refreshToken }, { withCredentials: true });
+
+                // Store the new JWT token
+                const newToken = response.data.token; // Adjust according to your API response
+                sessionStorage.setItem('jwtToken', newToken);
+
+                // Update the original request with the new token
+                originalRequest.headers['Authorization'] = `Bearer ${newToken}`;
+
+                return api(originalRequest);
+
+            } catch (refreshError) {
+                console.error('Error during token refresh:', refreshError);
+                return Promise.reject(refreshError);
+            }
+        }
+        return Promise.reject(error);
+    }
+
+    api.interceptors.request.use(onRequest, onRequestError);
+    api.interceptors.response.use(onResponse, onResponseError);
+
+    return api;
+})();
--- src/pages/authentication/api/authentication.ts
+++ src/pages/authentication/api/authentication.ts
@@ -1,9 +1,29 @@
+import { service } from "../../../core/apis/axiosInstance";
 import { IAuthentication } from "../interface";
 
-export const loginService = (body: IAuthentication) => {
+export const loginService = async (body: IAuthentication) => {
     try {
-        console.log(body, "creation body!!")
+        const response = await service.post("", body);
+        console.log(response?.data, "response!!")
     } catch (error) {
+        console.log(error, "response error!!")
+    }
+}
 
+export const requestPasswordResetService = async (body: IAuthentication) => {
+    try {
+        const response = await service.post("", body);
+        console.log(response?.data, "response!!");
+    } catch (error) {
+        console.log(error, "response error!!")
+    }
+}
+
+export const resetPasswordService = async (body: IAuthentication) => {
+    try {
+        const response = await service.post("", body);
+        console.log(response?.data, "response!!");
+    } catch (error) {
+        console.log(error, "response error!!")
     }
 }
