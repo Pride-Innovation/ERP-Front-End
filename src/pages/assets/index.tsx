cc3736d25593f41d762317a8485d8b0520ead14f
Sunday Odong <sodong@pridemicrofinance.co.ug>
2024-10-16 12:57:44 +0300

    fix layout

--- src/pages/assets/index.tsx
+++ src/pages/assets/index.tsx
@@ -11,6 +11,9 @@ import {
 } from 'react-router';
 import { ROUTES } from '../../core/routes/routes';
 import { useEffect, useState } from 'react';
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness';
+import BalanceIcon from '@mui/icons-material/Balance';
+import DirectionsCarFilledIcon from '@mui/icons-material/DirectionsCarFilled';
 
 const AssetsManagement = () => {
   const [path, setPath] = useState<string>("");
@@ -19,21 +22,24 @@ const AssetsManagement = () => {
 
   useEffect(() => { setPath(pathname) }, [pathname])
 
-  const navigations: Array<{ id: number, text: string, path: string }> = [
+  const navigations: Array<{ id: number, text: string, path: string, icon: JSX.Element }> = [
     {
       id: 1,
       text: "IT Equipment",
-      path: ROUTES.LIST_ASSETS
+      path: ROUTES.LIST_ASSETS,
+      icon: <SettingsBrightnessIcon />
     },
     {
       id: 2,
       text: "Office Equipment",
-      path: ROUTES.LIST_OFFICE_EQUIPMENT
+      path: ROUTES.LIST_OFFICE_EQUIPMENT,
+      icon: <BalanceIcon />
     },
     {
       id: 3,
       text: "Fleet",
-      path: ROUTES.LIST_FLEET
+      path: ROUTES.LIST_FLEET,
+      icon: <DirectionsCarFilledIcon />
     }
   ]
 
@@ -41,9 +47,10 @@ const AssetsManagement = () => {
     <>
       <Card sx={{ p: 2, mb: 2 }}>
         <Grid xs={12} container>
-          <Stack direction="row" spacing={4}>
+          <Stack direction="row" spacing={1}>
             {navigations.map(item => (
               <Button
+                startIcon={item.icon}
                 onClick={() => navigate(item.path)}
                 key={item.id}
                 variant={[path].includes(item.path) ? "contained" : "outlined"}>
