254f44db8719257617d99e3137f20272b6adb42a
Sunday Odong <sodong@pridemicrofinance.co.ug>
2024-10-21 12:09:41 +0300

    fix layout

--- src/pages/assets/interface.ts
+++ src/pages/assets/interface.ts
@@ -5,15 +5,19 @@ export interface IFormData<T> {
     value: Path<T>;
     label: string;
     type: "input" | "select" | "date" | "autocomplete";
-    options?: Array<IOptions>
+    options?: Array<IOptions>;
 }
 
 export interface IDispose {
     handleClose: () => void;
     sendingRequest: boolean;
-    buttonText: string
+    buttonText: string;
 }
 
 export interface INavigation {
-    id: number, text: string, path: string, icon: JSX.Element, otherRoutes: Array<string>
+    id: number;
+    text: string;
+    path: string;
+    icon: JSX.Element;
+    otherRoutes: Array<string>;
 }
