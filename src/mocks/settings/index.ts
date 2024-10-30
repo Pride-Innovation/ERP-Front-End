d2a0543cabff2ebe0f71c87562f6c3654d46b638
Odong Sunday <sandieo.2020@gmail.com>
2024-10-30 16:50:56 +0300

    fix layout

--- src/mocks/settings/index.ts
+++ src/mocks/settings/index.ts
@@ -1,13 +1,12 @@
 import { IPermission, IRole } from "../../pages/settings/interface";
 
 export const permissionsMock: IPermission[] = [
-    { id: 1, name: "read_user" },
-    { id: 2, name: "create_user" },
-    { id: 3, name: "delete_user" },
-    { id: 4, name: "update_user" },
+    { id: 1, name: "read_users" },
+    { id: 2, name: "create_users" },
+    { id: 3, name: "delete_users" },
+    { id: 4, name: "update_users" },
     { id: 5, name: "update_it_equipment" },
     { id: 6, name: "create_it_equipment" },
-    { id: 7, name: "delete_it_equipment" },
     { id: 8, name: "read_it_equipment" },
     { id: 9, name: "update_office_equipment" },
     { id: 10, name: "create_office_equipment" },
@@ -24,33 +23,33 @@ export const rolesMock: IRole[] = [
         id: 1,
         name: "System Administrator",
         permissions: [
-            { id: 1, name: "read_user" },
-            { id: 2, name: "create_user" },
-            { id: 3, name: "delete_user" },
-            { id: 4, name: "update_user" },
+            { id: 1, name: "read_users" },
+            { id: 2, name: "create_users" },
             { id: 5, name: "update_it_equipment" },
             { id: 6, name: "create_it_equipment" },
             { id: 7, name: "delete_it_equipment" },
             { id: 8, name: "read_it_equipment" },
-            { id: 9, name: "update_office_equipment" },
+            { id: 10, name: "create_office_equipment" },
+            { id: 11, name: "delete_office_equipment" },
+            { id: 12, name: "read_office_equipment" },
+            { id: 13, name: "update_fleet" },
+            { id: 14, name: "create_fleet" },
         ]
     },
     {
         id: 2,
         name: "Admin Officer",
         permissions: [
-            { id: 9, name: "update_office_equipment" },
+            { id: 1, name: "read_users" },
+            { id: 2, name: "create_users" },
+            { id: 3, name: "delete_users" },
+            { id: 4, name: "update_users" },
+            { id: 5, name: "update_it_equipment" },
+            { id: 6, name: "create_it_equipment" },
+            { id: 8, name: "read_it_equipment" },
             { id: 10, name: "create_office_equipment" },
             { id: 11, name: "delete_office_equipment" },
             { id: 12, name: "read_office_equipment" },
-            { id: 7, name: "delete_it_equipment" },
-            { id: 13, name: "update_fleet" },
-        ]
-    },
-    {
-        id: 3,
-        name: "ICT Officer",
-        permissions: [
             { id: 13, name: "update_fleet" },
             { id: 14, name: "create_fleet" },
             { id: 15, name: "delete_fleet" },
@@ -58,13 +57,38 @@ export const rolesMock: IRole[] = [
         ]
     },
     {
-        id: 4,
-        name: "Finance Officer",
+        id: 3,
+        name: "ICT Officer",
         permissions: [
-            { id: 5, name: "update_it_equipment" },
+            { id: 1, name: "read_users" },
+            { id: 2, name: "create_users" },
+            { id: 3, name: "delete_users" },
+            { id: 4, name: "update_users" },
             { id: 6, name: "create_it_equipment" },
             { id: 7, name: "delete_it_equipment" },
             { id: 8, name: "read_it_equipment" },
+            { id: 9, name: "update_office_equipment" },
+            { id: 10, name: "create_office_equipment" },
+            { id: 12, name: "read_office_equipment" },
+            { id: 13, name: "update_fleet" },
+            { id: 14, name: "create_fleet" },
+            { id: 16, name: "read_fleet" },
+        ]
+    },
+    {
+        id: 4,
+        name: "Finance Officer",
+        permissions: [
+            { id: 1, name: "read_users" },
+            { id: 3, name: "delete_users" },
+            { id: 4, name: "update_users" },
+            { id: 5, name: "update_it_equipment" },
+            { id: 8, name: "read_it_equipment" },
+            { id: 9, name: "update_office_equipment" },
+            { id: 12, name: "read_office_equipment" },
+            { id: 13, name: "update_fleet" },
+            { id: 15, name: "delete_fleet" },
+            { id: 16, name: "read_fleet" },
         ]
     },
     {
@@ -72,17 +96,30 @@ export const rolesMock: IRole[] = [
         name: "Officer",
         permissions: [
             { id: 1, name: "read_users" },
-            { id: 2, name: "create_users" },
             { id: 3, name: "delete_users" },
+            { id: 4, name: "update_users" },
+            { id: 7, name: "delete_it_equipment" },
+            { id: 9, name: "update_office_equipment" },
+            { id: 10, name: "create_office_equipment" },
+            { id: 11, name: "delete_office_equipment" },
+            { id: 13, name: "update_fleet" },
+            { id: 16, name: "read_fleet" },
         ]
     },
     {
         id: 6,
         name: "Supervisor",
         permissions: [
+            { id: 2, name: "create_users" },
+            { id: 3, name: "delete_users" },
+            { id: 5, name: "update_it_equipment" },
+            { id: 6, name: "create_it_equipment" },
             { id: 7, name: "delete_it_equipment" },
-            { id: 8, name: "read_it_equipment" },
             { id: 9, name: "update_office_equipment" },
+            { id: 11, name: "delete_office_equipment" },
+            { id: 12, name: "read_office_equipment" },
+            { id: 13, name: "update_fleet" },
+            { id: 16, name: "read_fleet" },
         ]
     }
 ];
