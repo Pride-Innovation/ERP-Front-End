54213a2d3ed8803f996d9998ac13903583c2178e
Odong Sunday <sandieo.2020@gmail.com>
2024-10-31 17:06:55 +0300

    fix layout

--- src/pages/settings/roles/index.tsx
+++ src/pages/settings/roles/index.tsx
@@ -33,56 +33,58 @@ const Roles = () => {
     }
 
     return (
-        <Box sx={{ py: 4 }}>
-            <Box sx={{
-                width: "100%",
-                display: "flex",
-                justifyContent: "space-between",
-                mb: 4,
-                alignItems: "center",
-            }}>
-                {
-                    crudStates.create === modalState && <ModalComponent width={"40%"} title='Create Role' open={open} handleClose={handleClose}>
-                        <CreateRole handleClose={handleClose} sendingRequest={false} />
-                    </ModalComponent>
-                }
-                {
-                    crudStates.delete === modalState && <ModalComponent width={"40%"} title='Delete Role' open={open} handleClose={handleClose}>
-                        <DeleteRole role={currentRole} handleClose={handleClose} sendingRequest={false} buttonText='Delete' />
-                    </ModalComponent>
-                }
-                {
-                    crudStates.update === modalState && <ModalComponent width={"40%"} title='Update Role' open={open} handleClose={handleClose}>
-                        <UpdateRole handleClose={handleClose} sendingRequest={false} role={currentRole} />
-                    </ModalComponent>
-                }
-                <Typography sx={{ fontWeight: 600, textTransform: "none", fontSize: '17px' }}>Accounts Settings</Typography>
-                <Box>
-                    <ButtonComponent
-                        handleClick={createRole}
-                        sendingRequest={false}
-                        buttonText="Create New Role"
-                        variant='contained'
-                        buttonColor='info'
-                        type='button' />
+        <>
+            {
+                crudStates.create === modalState && <ModalComponent width={"40%"} title='Create Role' open={open} handleClose={handleClose}>
+                    <CreateRole handleClose={handleClose} sendingRequest={false} />
+                </ModalComponent>
+            }
+            {
+                crudStates.delete === modalState && <ModalComponent width={"40%"} title='Delete Role' open={open} handleClose={handleClose}>
+                    <DeleteRole role={currentRole} handleClose={handleClose} sendingRequest={false} buttonText='Delete' />
+                </ModalComponent>
+            }
+            {
+                crudStates.update === modalState && <ModalComponent width={"40%"} title='Update Role' open={open} handleClose={handleClose}>
+                    <UpdateRole handleClose={handleClose} sendingRequest={false} role={currentRole} />
+                </ModalComponent>
+            }
+            <Box sx={{ py: 4 }}>
+                <Box sx={{
+                    width: "100%",
+                    display: "flex",
+                    justifyContent: "space-between",
+                    mb: 4,
+                    alignItems: "center",
+                }}>
+                    <Typography sx={{ fontWeight: 600, textTransform: "none", fontSize: '17px' }}>Accounts Settings</Typography>
+                    <Box>
+                        <ButtonComponent
+                            handleClick={createRole}
+                            sendingRequest={false}
+                            buttonText="Create New Role"
+                            variant='contained'
+                            buttonColor='info'
+                            type='button' />
+                    </Box>
                 </Box>
-            </Box>
-            <Box
-                display="grid"
-                sx={{ width: "100%" }}
-                gridTemplateColumns="1fr"
-                gap={4}
-            >
-                {
-                    rolesMock.map((role: IRole) => {
-                        return (
-                            <RoleDetails updateRole={updateRole} deleteRole={deleteRole} role={role} />
-                        )
-                    })
-                }
-            </Box>
+                <Box
+                    display="grid"
+                    sx={{ width: "100%" }}
+                    gridTemplateColumns="1fr"
+                    gap={4}
+                >
+                    {
+                        rolesMock.map((role: IRole) => {
+                            return (
+                                <RoleDetails updateRole={updateRole} deleteRole={deleteRole} role={role} />
+                            )
+                        })
+                    }
+                </Box>
 
-        </Box>
+            </Box>
+        </>
     )
 }
 
