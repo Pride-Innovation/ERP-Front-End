1cf8c5407ac43830d76657b307d3768f0cc848f2
Odong Sunday <sandieo.2020@gmail.com>
2025-05-08 12:33:40 +0300

    fix modal

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -2,17 +2,7 @@ import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import { IModalComponent } from './interface';
 import { TypographyComponent } from '../headers/TypographyComponent';
-import { Divider } from '@mui/material';
-
-const style = {
-    position: 'absolute' as 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    bgcolor: 'background.paper',
-    py: 4,
-    borderRadius: "4px"
-};
+import { Divider, useTheme } from '@mui/material';
 
 export default function ModalComponent({
     children,
@@ -21,24 +11,49 @@ export default function ModalComponent({
     width = "40%",
     title
 }: IModalComponent) {
+    const theme = useTheme();
+
     return (
-        <div>
-            <Modal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
+        <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-title"
+            aria-describedby="modal-description"
+        >
+            <Box
+                sx={{
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                    width: { xs: '90%', sm: width },
+                    bgcolor: theme.palette.background.paper,
+                    borderRadius: 2,
+                    boxShadow: 24,
+                    px: { xs: 3, sm: 4 },
+                    py: 4,
+                }}
             >
-                <Box sx={{ ...style, width }}>
-                    <TypographyComponent sx={{ mb: 3, px: 4, textTransform: "uppercase" }} size='17px' weight={600} >
-                        {title}
-                    </TypographyComponent>
-                    <Divider sx={{ my: 3 }} />
-                    <Box sx={{ width: "100%", px: 4 }}>
-                        {children}
-                    </Box>
+                <TypographyComponent
+                    id="modal-title"
+                    size="18px"
+                    weight={700}
+                    sx={{
+                        mb: 1,
+                        textTransform: 'uppercase',
+                        color: theme.palette.secondary.main,
+                        letterSpacing: 0.5,
+                    }}
+                >
+                    {title}
+                </TypographyComponent>
+
+                <Divider sx={{ mb: 3, borderColor: theme.palette.grey[300] }} />
+
+                <Box id="modal-description">
+                    {children}
                 </Box>
-            </Modal>
-        </div>
+            </Box>
+        </Modal>
     );
 }
