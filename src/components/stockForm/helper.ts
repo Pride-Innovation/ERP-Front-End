41e0c4ebb230aea7352f7fb85cb7604570040534
Odong Sunday <sandieo.2020@gmail.com>
2025-06-07 12:29:44 +0300

    fix copyright

--- src/components/stockForm/helper.ts
+++ src/components/stockForm/helper.ts
@@ -1,3 +1,10 @@
+/*
+13.9 Pride's Standard Copyright Notice:
+Copyright ©20XX. Management of Pride Bank Limited (PBL). All Rights Reserved. Permission to use, copy, modify, 
+and distribute this software and its documentation for any purpose is prohibited unless authorized in writing by the
+Managing Director
+*/
+
 export const formatNumberWithCommas = (value: number | string): string => {
     if (value === '' || isNaN(Number(value))) return '';
     return Number(value).toLocaleString('en-UG');
