dd83043237497f3320cab7cec0b5aaadde270227
OdongAlican <sandieo.2020@gmail.com>
2026-04-13 12:04:35 +0300

    update schema

--- src/pages/movement/schema.ts
+++ src/pages/movement/schema.ts
@@ -9,8 +9,11 @@ import * as yup from 'yup';
 
 export const movementSchema = yup.object().shape({
     officerId: yup.mixed().required('Requesting officer is required'),
+    approverId: yup.mixed().nullable().optional(),
     destination: yup.string().required('Destination is required'),
+    destinationId: yup.mixed().nullable().optional(),
     destinationType: yup.string().required('Destination type is required'),
+    assetIds: yup.array().required().min(1, 'Please add at least one asset to move'),
     reason: yup.string().required('Reason is required'),
     expectedReturnDate: yup.string().nullable().optional(),
 });
