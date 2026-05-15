646fb770b2f2dfcc8b33ba0694f310a2a43dea06
OdongAlican <sandieo.2020@gmail.com>
2026-05-15 09:07:26 +0300

    update supplier schema

--- src/pages/settings/suppliers/schema.ts
+++ src/pages/settings/suppliers/schema.ts
@@ -10,6 +10,10 @@ import * as yup from 'yup';
 export const supplierSchema = yup.object().shape({
     name: yup.string().required('Name is required'),
     email: yup.string().trim().email('Invalid email format').required('Email Address is required'),
-    telephone: yup.string().trim().required('Telephone Number is required'),
+    telephone: yup.string()
+        .trim()
+        .matches(/^\+256\d{9}$/, 'Must be a valid Ugandan number (e.g. +256700000000)')
+        .required('Telephone Number is required'),
     address: yup.string().nullable().optional(),
+    commodities: yup.array().of(yup.number().required()).nullable().optional(),
 });
