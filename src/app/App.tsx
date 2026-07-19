34d224781a10d7f41b57d29c99fe8610e721c858
Odong Sunday <sandieo.2020@gmail.com>
2026-07-19 10:49:42 +0300

    update app index

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,12 @@
 /*
 13.9 Pride's Standard Copyright Notice:
-Copyright ©20XX. Management of Pride Bank Limited (PBL). All Rights Reserved. Permission to use, copy, modify, 
+Copyright ©20XX. Management of Pride Bank Limited (PBL). All Rights Reserved. Permission to use, copy, modify,
 and distribute this software and its documentation for any purpose is prohibited unless authorized in writing by the
 Managing Director
 */
 
-import { BrowserRouter as Router } from "react-router-dom";
-import AppRoutes from "../core/routes/AppRoutes";
+import { RouterProvider } from "react-router-dom";
+import router from "../core/routes/AppRoutes";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -15,9 +15,7 @@ const App = () => {
   return (
     <>
       <ToastContainer />
-      <Router>
-        <AppRoutes />
-      </Router>
+      <RouterProvider router={router} />
     </>
 
   );
